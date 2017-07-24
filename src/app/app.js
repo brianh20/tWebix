72d4cee15562f31fb513c5f9861d58ddbb58668c
Brian Hume <brian.hume@synapse.com>
2017-07-24 16:22:46 -0300

    Began mock data setup

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,16 @@
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     console.log(JSON.parse(this.responseText));
+    }
+  };
+  xhttp.open("GET", "../../mock/site.json", true);
+  xhttp.send();
+}
+
+loadDoc();
+
 
 webix.ui({
 	type:"line",
